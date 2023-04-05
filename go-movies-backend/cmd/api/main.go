8c8dbb050dc8a1ba3d3e73cdcbd1b1cf05e1e06f
Mert Armagan <mert.armagan@outlook.com>
2023-04-04 17:29:36 -0700

    Adding routes to handlers on backend

--- go-movies-backend/cmd/api/main.go
+++ go-movies-backend/cmd/api/main.go
@@ -23,10 +23,9 @@ func main() {
 
 	log.Println("Starting application on port", port)
 
-	http.HandleFunc("/", Hello)
-
 	// Start a web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	
 	if err != nil {
 		log.Fatal(err)
 	}
--- go-movies-backend/cmd/api/routes.go
+++ go-movies-backend/cmd/api/routes.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	// Create router multiplexer
+	mux := chi.NewRouter()
+
+	// Log backtrace if error occurs and recover page content
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", app.Home)
+
+	return mux
+}

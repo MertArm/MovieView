a1e84e40700716899f69a65b25983e0b1ea02447
Mert Armagan <mert.armagan@outlook.com>
2023-05-08 23:21:31 -0700

    Committing repository and model of backend

--- go-movies-backend/internal/repository/repository.go
+++ go-movies-backend/internal/repository/repository.go
@@ -8,4 +8,10 @@ import (
 type DatabaseRepo interface {
 	Connection() *sql.DB
 	AllMovies() ([]*models.Movie, error)
+	GetUserByEmail(email string) (*models.User, error)
+	GetUserById(id int) (*models.User, error)
+	OneMovie(id int) (*models.Movie, error)
+	OneMovieForEdit(id int) (*models.Movie, []*models.Genre, error)
+	AllGenres() ([]*models.Genre, error)
+	InsertMovie(movie models.Movie) (int, error)
 }
